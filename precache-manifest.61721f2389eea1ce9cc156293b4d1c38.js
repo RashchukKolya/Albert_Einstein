50120d7e670a44d598bd93cda49d908c1c1b5b9c
Nick <raschyk1993@gmail.com>
2019-03-06 15:51:30 +0200

    Updates

---
+++ precache-manifest.61721f2389eea1ce9cc156293b4d1c38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "43e7a1c494a157a27ede",
+    "url": "/Albert_Einstein/static/js/runtime~main.43e7a1c4.js"
+  },
+  {
+    "revision": "348ae358d9fd0361983d",
+    "url": "/Albert_Einstein/static/js/main.348ae358.chunk.js"
+  },
+  {
+    "revision": "95df7cd3697ff2b77927",
+    "url": "/Albert_Einstein/static/js/2.95df7cd3.chunk.js"
+  },
+  {
+    "revision": "348ae358d9fd0361983d",
+    "url": "/Albert_Einstein/static/css/main.77210a85.chunk.css"
+  },
+  {
+    "revision": "ad56dd53527a5e80dbee230c121643d9",
+    "url": "/Albert_Einstein/index.html"
+  }
+];
